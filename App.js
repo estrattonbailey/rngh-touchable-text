aebca5d29d353fd1a4e4541f0abec64eed9eda08
Samuel Newman <mozzius@protonmail.com>
2024-10-03 16:04:16 +0300

    add repro

--- App.js
+++ App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Pressable as RNPressable,
+} from 'react-native';
+
+import {
+  Pressable as RNGHPressable,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView>
+        <View style={{ padding: 16, gap: 8 }}>
+          <Text>default Pressable</Text>
+          <View style={{ flexDirection: 'row', gap: 8 }}>
+            <RNPressable
+              style={[
+                buttonStyle,
+                {
+                  backgroundColor: 'lightgreen',
+                  borderRadius: 16,
+                },
+              ]}>
+              <Text>16</Text>
+            </RNPressable>
+            <RNPressable
+              style={[
+                buttonStyle,
+                {
+                  backgroundColor: 'lightgreen',
+                  borderRadius: 32,
+                },
+              ]}>
+              <Text>32</Text>
+            </RNPressable>
+            <RNPressable
+              style={[
+                buttonStyle,
+                {
+                  backgroundColor: 'lightgreen',
+                  borderRadius: 999,
+                },
+              ]}>
+              <Text>999</Text>
+            </RNPressable>
+          </View>
+          <Text>RNGH Pressable</Text>
+          <View style={{ flexDirection: 'row', gap: 8 }}>
+            <RNGHPressable
+              style={[
+                buttonStyle,
+                {
+                  backgroundColor: 'lightgreen',
+                  borderRadius: 16,
+                },
+              ]}>
+              <Text>16</Text>
+            </RNGHPressable>
+            <RNGHPressable
+              style={[
+                buttonStyle,
+                {
+                  backgroundColor: 'lightcoral',
+                  borderRadius: 32,
+                },
+              ]}>
+              <Text>32</Text>
+            </RNGHPressable>
+            <RNGHPressable
+              style={[
+                buttonStyle,
+                {
+                  backgroundColor: 'lightcoral',
+                  borderRadius: 999,
+                },
+              ]}>
+              <Text>999</Text>
+            </RNGHPressable>
+          </View>
+        </View>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const buttonStyle = {
+  width: 32,
+  height: 32,
+  alignItems: 'center',
+  justifyContent: 'center',
+};
